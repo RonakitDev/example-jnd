8b00777c7e067f9ea6b0be18fa674e38ad42444d
RonakitDev <70593116+RonakitDev@users.noreply.github.com>
2024-10-09 16:46:40 +0700

    firstpage

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\ShotlinkController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('shotlink', MainController::class);
 Route::get('/data', [MainController::class, 'getData'])->name('data.get');
 Route::get('/jdntest/{id}', [MainController::class, 'openlink']);
